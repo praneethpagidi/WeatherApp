f4547f159c56fda8c5fa09a54fa92535230108cc
Praneeth <praneeth.pagidi@united.com>
2019-10-15 10:00:52 -0500

    Initial Commit

---
+++ app/src/main/java/praneeth/com/sample/adapters/ForecastListAdapater.kt
@@ -0,0 +1,26 @@
+package praneeth.com.sample.adapters
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by Praneeth on 2019-10-15.
+ */
+class ForecastListAdapater(val items: List<String>) :
+    RecyclerView.Adapter<ForecastListAdapater.ViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder =
+        ViewHolder(TextView(parent.context))
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+}

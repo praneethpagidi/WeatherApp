1f58fbaa18d2b4c44e25e897216ffccfe8859b19
Praneeth <praneeth.pagidi@united.com>
2019-10-17 17:28:51 -0500

    [Praneeth] - Added a dataMapper class, mapping response to the data needed

--- app/src/main/java/praneeth/com/sample/domain/data/Forecast.kt
+++ app/src/main/java/praneeth/com/sample/domain/data/Forecast.kt
@@ -4,7 +4,7 @@ package praneeth.com.sample.domain.data
  * Created by Praneeth on 2019-10-17.
  */
 data class Forecast(
-    val date: String,
+    val day: String,
     val description: String,
     val high: Int,
     val low: Int
--- app/src/main/java/praneeth/com/sample/domain/mappers/ForecastDataMapper.kt
+++ app/src/main/java/praneeth/com/sample/domain/mappers/ForecastDataMapper.kt
@@ -1,11 +1,33 @@
 package praneeth.com.sample.domain.mappers
 
+
+import android.util.Log
+import praneeth.com.sample.domain.data.Forecast
+import praneeth.com.sample.domain.data.ForecastData
+import java.util.*
+import kotlin.collections.ArrayList
 /**
  * Created by Praneeth on 2019-10-17.
  */
 class ForecastDataMapper {
 
-//    fun convertToForecastList(forecastData: ForecastData) : ForecastList {
-//        ForecastList(forecastData.city)
-//    }
+    private val daysList = listOf("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+
+    fun convertToWeeklyForecastList(forecastData: ForecastData) : List<Forecast> {
+        val forecastList: ArrayList<Forecast> = ArrayList()
+        var dayCount = Calendar.getInstance()[Calendar.DAY_OF_WEEK] - 1
+
+        for (data in forecastData.daily.data) {
+            if (dayCount == 7) {
+                dayCount = 0
+            }
+
+            val today = daysList.get(dayCount)
+            Log.d("current day", today)
+            val forecast = Forecast(today, data.summary, data.temperatureHigh.toInt(), data.temperatureLow.toInt())
+            forecastList.add(forecast)
+            dayCount++
+        }
+        return forecastList
+    }
 }
